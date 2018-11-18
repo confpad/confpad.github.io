00bc90c6dd0e850a5c74486caa6e58b4ee7f3d87
Peta Sittek <peta.sittek@gmail.com>
2018-11-19 00:41:23 +0100

    Sort conferences by data.from

--- js/reducers/conferenceListReducer.js
+++ js/reducers/conferenceListReducer.js
@@ -8,18 +8,27 @@ const INITIAL_STATE = {
   isFetching: false,
 };
 
+const sortByDateFrom = (a, b) => {
+  if (a.date.from < b.date.from) {
+    return 1;
+  } else if (b.date.from < a.date.from) {
+    return -1;
+  }
+  return 0;
+};
+
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ACTION_CONFERENCE_LIST_IS_FETCHING:
       return {
         ...state,
-        isFetching: true
+        isFetching: true,
       };
     case ACTION_CONFERENCE_LIST_FETCHED:
       return {
         ...state,
         isFetching: false,
-        data: action.payload
+        data: action.payload.sort(sortByDateFrom),
       };
   }
 
