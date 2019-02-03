d96d1c7240e43c8a02d79c076a46bc307800f23b
Peta Sittek <peta.sittek@gmail.com>
2019-02-04 00:40:04 +0100

    Fix tests and make them verbose

--- test/conference-details.test.js
+++ test/conference-details.test.js
@@ -6,7 +6,7 @@ const jsyaml = require('js-yaml');
 const ROOT_KEYS = ['title', 'type', 'level', 'time', 'room', 'authors', 'slides', 'videos', 'description'];
 const AUTHOR_KEYS = ['name', 'twitter', 'github', 'website'];
 
-glob.sync('{./data/conferences/*.yaml,./examples/2018-01-01-some-cool-conference.yaml}').forEach(file => {
+glob.sync('{./data/conferences/*/*.yaml,./examples/2018-01-01-some-cool-conference.yaml}').forEach(file => {
   let talks;
 
   // Read YAML config
