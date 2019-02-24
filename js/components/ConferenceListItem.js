b7731bb44fac0907b66067d8ac44d22ce727c6dd
Peta Sittek <peta.sittek@gmail.com>
2019-02-24 13:56:04 +0100

    Add microdata to conference list item

--- js/components/ConferenceListItem.js
+++ js/components/ConferenceListItem.js
@@ -27,36 +27,49 @@ const getTitle = (id, name, isDetail) => {
 
   return html`
     <h2 class="${CLASS_LINE_MV} f4 fw6">
-      <a href="/${id}" class="link underline-hover">${name}</a>
+      <a href="/${id}" class="link underline-hover">
+        <span itemprop="name">${name}</span>
+      </a>
     </h2>
   `
 };
 
-const getDate = (from, to) => html`
-  <div class="${CLASS_LINE_INFO_DISPLAY} ${CLASS_LINE_MV} ${CLASS_LINE_INFO_MR}">
-    📅
-    ${from && from.toISOString().split('T')[0]}
-  </div>
-`;
+const getDate = (from, to) => {
+  let fromDate = from && from.toISOString().split('T')[0];
 
-const getLocation = location => html`
-  <div class="${CLASS_LINE_INFO_DISPLAY} ${CLASS_LINE_MV} ${CLASS_LINE_INFO_MR}">
-    🌎
-    ${[location.city, location.country].filter(item => item).join(', ')}
-  </div>
-`;
+  return html`
+    <div class="${CLASS_LINE_INFO_DISPLAY} ${CLASS_LINE_MV} ${CLASS_LINE_INFO_MR}">
+      📅
+      <span itemprop="startDate" content="${fromDate}">${fromDate}</span>
+    </div>
+  `;
+};
+
+const getLocation = location => {
+  return html`
+    <div class="${CLASS_LINE_INFO_DISPLAY} ${CLASS_LINE_MV} ${CLASS_LINE_INFO_MR}">
+      🌎
+      <span itemprop="location" itemscope itemtype="http://schema.org/Place">
+        <span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+          <span itemprop="addressLocality">${location.city}</span>,
+          <span itemprop="addressCountry">${location.country}</span>
+        </span>
+      </span>
+    </div>
+  `
+};
 
 const getLink = url => html`
   <div class="${CLASS_LINE_INFO_DISPLAY} ${CLASS_LINE_MV} ${CLASS_LINE_INFO_MR}">
     🔗
-    <a href="${url}" target="_blank" rel="noopener">
+    <a href="${url}" target="_blank" rel="noopener" itemprop="url">
       ${url}
     </a>
   </div>
 `;
 
 const getDescription = description => html`
-  <div class="mv1 ${CLASS_LINE_MV} gray">
+  <div class="mv1 ${CLASS_LINE_MV} gray" itemprop="description">
     ${description}
   </div>
 `;
@@ -65,7 +78,7 @@ const ConferenceListItem = props => {
   let elTag = props.isDetail ? 'div' : 'li';
 
   return html`
-    <${elTag} class="${props.isDetail ? 'mt4 mb3' : 'mv4'}">
+    <${elTag} class="${props.isDetail ? 'mt4 mb3' : 'mv4'}" itemscope itemtype="http://schema.org/Event">
       ${getTitle(props.id, props.name, props.isDetail)}
       <div class="truncate">
         ${props.date && getDate(props.date.from, props.date.to)}
