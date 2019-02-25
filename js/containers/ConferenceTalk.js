962aea5ee5cbae99748536b8cc639db4a756e228
Peta Sittek <peta.sittek@gmail.com>
2019-02-25 23:25:39 +0100

    Add light gray background to talk videos

--- js/containers/ConferenceTalk.js
+++ js/containers/ConferenceTalk.js
@@ -56,7 +56,7 @@ class ConferenceDetail extends Component {
             let videoId = match[1];
 
             return html`
-              <div class="mv3 w-100 aspect-ratio aspect-ratio--16x9">
+              <div class="mv3 w-100 aspect-ratio aspect-ratio--16x9 bg-light-gray">
                 <iframe class="aspect-ratio--object" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
             `
@@ -68,7 +68,7 @@ class ConferenceDetail extends Component {
             let videoId = match[1];
 
             return html`
-              <div class="mv3 w-100 aspect-ratio aspect-ratio--16x9">
+              <div class="mv3 w-100 aspect-ratio aspect-ratio--16x9 bg-light-gray">
                 <iframe class="aspect-ratio--object" src="https://player.vimeo.com/video/${videoId}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
               </div>
             `
